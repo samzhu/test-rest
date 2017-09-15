746d3e5585cdd49a0c858a4761f1ac5ec3803910
sam <spike19820318@gmail.com>
2017-09-15 11:25:10 +0800

    first-test

---
+++ src/main/java/com/example/demo/TestControllert.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("api")
+public class TestControllert {
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(value = "v1/test")
+    public Map<String, String> test() {
+        Map<String, String> data = new HashMap<String, String>();
+        data.put("rc", "1");
+        return data;
+    }
+}
